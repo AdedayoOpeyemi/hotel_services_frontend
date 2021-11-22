99cda59a7bf4b4a13cbb20468199bdbee885cefe
Andres Castañeda <af.castaneda223@gmail.com>
2021-11-22 16:23:11 -0500

    add sidebar component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Provider } from 'react-redux';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
+import Navbar from './components/Navbar';
 import store from './redux/configurateStore';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        < Navbar />
         <BrowserRouter>
           <Routes>
             <Route exact path="/" element={<LoginForm />} />
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className="row vh-100 w-25 border sidebar">
+            <h1>Services</h1>
+        </div>
+    )
+}
+
+export default Navbar
