6e4867a52cb58a1d1f39193ef0ce4417e0169bb8
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-11-22 12:08:22 -0300

    some modifications

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import { Provider } from 'react-redux';
+import { Provider, useDispatch } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
 import LoginForm from './components/LoginForm';
 import store from './redux/configurateStore';
+import { checkLogin } from './redux/user/user_duck';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => dispatch(checkLogin));
+
   return (
     <Provider store={store}>
       <div className="App">
