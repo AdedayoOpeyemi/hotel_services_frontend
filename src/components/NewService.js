b79beda7a34fdd0fb6ab8ab7cc6aedc9ec2202d3
Andres Castañeda <af.castaneda223@gmail.com>
2021-11-22 17:24:48 -0500

    add simple scaffold for new form

--- src/components/NewService.js
+++ src/components/NewService.js
@@ -2,9 +2,43 @@ import React from 'react';
 
 function NewService() {
   return (
-    <h1 className="text-center">
-      Add new service
-    </h1>
+    <div className="container">
+      <h1>Book a new Service</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis nobis harum unde
+        consectetur velit rerum iure mollitia quia itaque ipsa error quo architecto assumenda, a
+        spernatur vel iste eos, illum quisquam!
+      </p>
+      <div className="row">
+        <div className="col">
+          <div className="dropdown">
+            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown button
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li> actions</li>
+              <li> actions</li>
+              <li> actions</li>
+            </ul>
+          </div>
+        </div>
+        <div className="col">
+          <div className="dropdown">
+            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown button
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+              <li> actions</li>
+              <li> actions</li>
+              <li> actions</li>
+            </ul>
+          </div>
+        </div>
+        <div className="col">
+          <button type="button" className="btn btn-dark">Dark</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
