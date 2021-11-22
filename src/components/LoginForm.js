5aaf640fb6ea189eb720a26667ae68e40a86bbba
Andres Castañeda <af.castaneda223@gmail.com>
2021-11-22 11:51:46 -0500

    linters fix

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import videos from '../components/assets/pool.mp4'
+import videos from './assets/pool.mp4';
 
 const LoginForm = () => {
   const inputTextField = React.createRef();
 
   const printInput = (name) => {
     console.log(name);
-  };  
+  };
 
   return (
     <header>
-      <video autoplay="autoplay" loop="loop" muted >
-        <source src={videos} type="video/mp4"/>
+      <video autoPlay="autoplay" loop="loop" muted>
+        <source src={videos} type="video/mp4" />
       </video>
-    <div className="container">
-      <div className="row vh-100 justify-content-center align-items-center">
-        <div className="col text-center text-white">
-          <h1>Welcome to City Services</h1>
-          <h5 className="fs-6 fw-normal">Please Log in or Sign in with your Username</h5>
-          <input type="text" className="form-control my-3" id="userName" aria-describedby="emailHelp" placeholder="User Name" ref={inputTextField} />
-          <button className="btn green" type="submit" onClick={() => printInput(inputTextField.current.value)}>Login/Sign in</button>
+      <div className="container">
+        <div className="row vh-100 justify-content-center align-items-center">
+          <div className="col text-center text-white">
+            <h1>Welcome to City Services</h1>
+            <h5 className="fs-6 fw-normal">Please Log in or Sign in with your Username</h5>
+            <input type="text" className="form-control my-3" id="userName" aria-describedby="emailHelp" placeholder="User Name" ref={inputTextField} />
+            <button className="btn green" type="submit" onClick={() => printInput(inputTextField.current.value)}>Login/Sign in</button>
+          </div>
         </div>
       </div>
-    </div>
     </header>
   );
 };
