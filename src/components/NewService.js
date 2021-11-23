2232d0b343dc537baa53d85e3f2a5b9655bc4d0e
Andres Castañeda <af.castaneda223@gmail.com>
2021-11-23 10:20:24 -0500

    add form for new service moove to booking

--- src/components/NewService.js
+++ src/components/NewService.js
@@ -6,94 +6,34 @@ function NewService() {
       <div className="container text-white">
         <div className="row vh-100 justify-content-center align-items-center">
           <div className="row">
-            <h1 className="text-center">Book a new Service</h1>
-            <p className="my-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis nobis harum unde
-              consectetur velit rerum iure mollitia quia itaque ipsa error quo
-              architecto assumenda, a spernatur vel iste eos, illum quisquam!
-            </p>
+            <h1 className="text-center mb-5">Create a new Service</h1>
+            <div className="row">
+              <div className="input-group mb-3">
+                <div className="input-group-prepend" />
+                <input type="text" className="form-control text-center rounded-pill mb-3" placeholder="Service name" aria-label="service_name" aria-describedby="basic-addon1" />
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-group mb-3">
+                <div className="input-group-prepend" />
+                <input type="text" className="form-control text-center rounded-pill mb-3" placeholder="Service Description" aria-label="Service_description" aria-describedby="basic-addon1" />
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-group mb-3">
+                <div className="input-group-prepend" />
+                <input type="number" className="form-control text-center rounded-pill mb-3" placeholder="Service Price" aria-label="Service_price" aria-describedby="basic-addon1" />
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-group mb-3">
+                <div className="input-group-prepend" />
+                <input type="text" className="form-control text-center rounded-pill mb-3" placeholder="Service image URL" aria-label="Service_image" aria-describedby="basic-addon1" />
+              </div>
+            </div>
             <div className="row">
               <div className="col">
-                <div className="input-group mb-3 ">
-                  <select className="form-select rounded-pill btng text-white" id="year">
-                    <option value="2021">2021</option>
-                    <option value="2022">2022</option>
-                    <option value="2023">2023</option>
-                    <option value="2021">2024</option>
-                    <option value="2022">2025</option>
-                    <option value="2023">2026</option>
-                  </select>
-                </div>
-              </div>
-              <div className="col ">
-                <div className="input-group mb-3">
-                  <select className="form-select rounded-pill btng text-white" id="month">
-                    <option value="01">January</option>
-                    <option value="02">February</option>
-                    <option value="03">March</option>
-                    <option value="04">April</option>
-                    <option value="05">May</option>
-                    <option value="06">June</option>
-                    <option value="01">July</option>
-                    <option value="02">August</option>
-                    <option value="03">September</option>
-                    <option value="04">October</option>
-                    <option value="05">November</option>
-                    <option value="06">December</option>
-                  </select>
-                </div>
-              </div>
-              <div className="col">
-                <div className="input-group mb-3">
-                  <select className="form-select rounded-pill btng text-white" id="day">
-                    <option value="01">01</option>
-                    <option value="02">02</option>
-                    <option value="03">03</option>
-                    <option value="04">04</option>
-                    <option value="05">05</option>
-                    <option value="06">06</option>
-                    <option value="01">07</option>
-                    <option value="02">08</option>
-                    <option value="03">09</option>
-                    <option value="04">10</option>
-                    <option value="05">11</option>
-                    <option value="06">12</option>
-                    <option value="01">13</option>
-                    <option value="02">14</option>
-                    <option value="03">15</option>
-                    <option value="04">16</option>
-                    <option value="05">17</option>
-                    <option value="06">18</option>
-                    <option value="01">19</option>
-                    <option value="02">20</option>
-                    <option value="03">21</option>
-                    <option value="04">22</option>
-                    <option value="05">23</option>
-                    <option value="06">24</option>
-                    <option value="02">25</option>
-                    <option value="03">26</option>
-                    <option value="04">27</option>
-                    <option value="05">28</option>
-                    <option value="06">29</option>
-                    <option value="05">30</option>
-                    <option value="06">31</option>
-                  </select>
-                </div>
-              </div>
-              <div className="col">
-                <div className="input-group mb-3 ">
-                  <select className="form-select rounded-pill btng text-white" id="city">
-                    <option value="London">London</option>
-                    <option value="Melbourne">Melbourne</option>
-                    <option value="Zurich">Zurich</option>
-                    <option value="Tokyo">Tokyo</option>
-                    <option value="Auckland">Auckland</option>
-                    <option value="New York">2026</option>
-                  </select>
-                </div>
-              </div>
-              <div className="col">
-                <button type="button" className="btn btn-light w-100 textg fw-bolder rounded-pill">Create</button>
+                <button type="button" className="btn btn-light w-100 textg fw-bolder rounded-pill mb-3">Create</button>
               </div>
             </div>
           </div>
