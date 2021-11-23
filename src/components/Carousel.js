e29489e904509ac62726bffbf4355f7e00cdff06
Andres Castañeda <af.castaneda223@gmail.com>
2021-11-23 15:41:42 -0500

    add serviceCard & Carousel to services

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,33 +1,13 @@
 import React from 'react';
-import office from './assets/office.jpeg';
-import pool from './assets/pool1.jpeg';
-import tennis from './assets/tennis.jpeg';
+import ServicesCard from './ServicesCard';
 
 function Carousel() {
   return (
     <div className="container w-75 mh-50">
       <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
-        <div className="carousel-inner">
+        <div className="carousel-inner ">
           <div className="carousel-item active">
-            <img src={office} className="d-block w-100" alt="..." />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>First slide label</h5>
-              <p>Some representative placeholder content for the first slide.</p>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img src={pool} className="d-block w-100" alt="..." />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Second slide label</h5>
-              <p>Some representative placeholder content for the second slide.</p>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img src={tennis} className="d-block w-100" alt="..." />
-            <div className="carousel-caption d-none d-md-block">
-              <h5>Third slide label</h5>
-              <p>Some representative placeholder content for the third slide.</p>
-            </div>
+            <ServicesCard />
           </div>
         </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
---
+++ src/components/ServicesCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import office from './assets/office.jpeg';
+
+function ServicesCard() {
+  return (
+<div>
+    <div className="card border-0 w-100 px-5">
+      <img src={office} className="img-fluid imgcont" alt="..." />
+      <div className="card-body text-center">
+        <h5 className="card-title text-center">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="stretched-link"></a>
+      </div>
+    </div> 
+</div>
+  );
+}
+
+export default ServicesCard;
