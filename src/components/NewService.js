9c756f3086f94c42e48535adb676e806a553d5b5
Andres Castañeda <af.castaneda223@gmail.com>
2021-11-23 11:16:06 -0500

    fix linters

--- src/components/NewService.js
+++ src/components/NewService.js
@@ -7,42 +7,42 @@ function NewService() {
       <video autoPlay="autoplay" loop="loop" muted>
         <source src={videos} type="video/mp4" />
       </video>
-        <div className="container text-white px-5">
-          <div className="row vh-100 justify-content-center align-items-center">
+      <div className="container text-white px-5">
+        <div className="row vh-100 justify-content-center align-items-center">
+          <div className="row">
+            <h1 className="text-center mb-5">Create a new Service</h1>
             <div className="row">
-              <h1 className="text-center mb-5">Create a new Service</h1>
-              <div className="row">
-                <div className="input-group mb-3">
-                  <div className="input-group-prepend" />
-                  <input type="text" className="form-control text-center rounded-pill mb-3" placeholder="Service name" aria-label="service_name" aria-describedby="basic-addon1" />
-                </div>
+              <div className="input-group mb-3">
+                <div className="input-group-prepend" />
+                <input type="text" className="form-control text-center rounded-pill mb-3" placeholder="Service name" aria-label="service_name" aria-describedby="basic-addon1" />
               </div>
-              <div className="row">
-                <div className="input-group mb-3">
-                  <div className="input-group-prepend" />
-                  <input type="text" className="form-control text-center rounded-pill mb-3" placeholder="Service Description" aria-label="Service_description" aria-describedby="basic-addon1" />
-                </div>
+            </div>
+            <div className="row">
+              <div className="input-group mb-3">
+                <div className="input-group-prepend" />
+                <input type="text" className="form-control text-center rounded-pill mb-3" placeholder="Service Description" aria-label="Service_description" aria-describedby="basic-addon1" />
               </div>
-              <div className="row">
-                <div className="input-group mb-3">
-                  <div className="input-group-prepend" />
-                  <input type="number" className="form-control text-center rounded-pill mb-3" placeholder="Service Price" aria-label="Service_price" aria-describedby="basic-addon1" />
-                </div>
+            </div>
+            <div className="row">
+              <div className="input-group mb-3">
+                <div className="input-group-prepend" />
+                <input type="number" className="form-control text-center rounded-pill mb-3" placeholder="Service Price" aria-label="Service_price" aria-describedby="basic-addon1" />
               </div>
-              <div className="row">
-                <div className="input-group mb-3">
-                  <div className="input-group-prepend" />
-                  <input type="text" className="form-control text-center rounded-pill mb-3" placeholder="Service image URL" aria-label="Service_image" aria-describedby="basic-addon1" />
-                </div>
+            </div>
+            <div className="row">
+              <div className="input-group mb-3">
+                <div className="input-group-prepend" />
+                <input type="text" className="form-control text-center rounded-pill mb-3" placeholder="Service image URL" aria-label="Service_image" aria-describedby="basic-addon1" />
               </div>
-              <div className="row">
-                <div className="col">
-                  <button type="button" className="btn text-white w-100 green fw-bolder rounded-pill mb-3">Create</button>
-                </div>
+            </div>
+            <div className="row">
+              <div className="col">
+                <button type="button" className="btn text-white w-100 green fw-bolder rounded-pill mb-3">Create</button>
               </div>
             </div>
           </div>
         </div>
+      </div>
     </header>
   );
 }
