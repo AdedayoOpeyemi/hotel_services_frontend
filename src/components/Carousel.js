ae5e89e71f12c5d678669d64a8a39355387f6381
Andres Castañeda <af.castaneda223@gmail.com>
2021-11-23 13:21:15 -0500

    add working caroulsel with demo img

---
+++ src/components/Carousel.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import office from './assets/office.jpeg';
+import pool from './assets/pool1.jpeg';
+import tennis from './assets/tennis.jpeg';
+
+function Carousel() {
+  return (
+    <div className="container w-75 mh-50">
+      <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={office} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>Some representative placeholder content for the first slide.</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src={pool} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>Some representative placeholder content for the second slide.</p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img src={tennis} className="d-block w-100" alt="..." />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>Some representative placeholder content for the third slide.</p>
+            </div>
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true" />
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true" />
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Carousel;
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,37 +1,18 @@
 import React from 'react';
+import Carousel from './Carousel';
 
 function Services() {
   return (
     <div className="container">
       <div className="row vh-100 justify-content-center align-items-center">
         <row>
-        <h1 className="text-center fw-bolder">
-          All Services
-        </h1>
-        <p className="fw-lighter text-center">
-          Please select a Service
-        </p>
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="https://picsum.photos/700" alt="First slide"/>
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="https://picsum.photos/600" alt="Second slide"/>
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="https://picsum.photos/700" alt="Third slide"/>
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+          <h1 className="text-center fw-bolder">
+            All Services
+          </h1>
+          <p className="fw-lighter text-center">
+            Please select a Service
+          </p>
+          <Carousel />
         </row>
       </div>
 
