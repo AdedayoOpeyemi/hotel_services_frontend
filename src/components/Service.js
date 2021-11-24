9641596b6899396416f6733e91f94242e5085fdc
Andres Castañeda <af.castaneda223@gmail.com>
2021-11-24 11:15:11 -0500

    add style to service

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,20 +1,32 @@
 import React from 'react';
+import office from './assets/office.jpeg';
 
 function Service() {
   return (
-    <div className="card mb-3">
-      <div className="row g-0">
-        <div className="col-md-4">
-          <img src="..." className="img-fluid rounded-start" alt="..." />
-        </div>
-        <div className="col-md-8">
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+    <div className="container">
+      <div className="row vh-100 justify-content-center align-items-center">
+        <div className="card mb-3 border-0">
+          <div className="row g-0">
+            <div className="col-6">
+              <img src={office} className="img-fluid rounded-start" alt="..." />
+            </div>
+            <div className="col-6">
+              <div className="card-body px-5">
+                <h2 className="card-title text-center mb-5">Service name</h2>
+                <ul class="list-group">
+                  <li class="list-group-item list-group-item-light">Service description</li>
+                  <li class="list-group-item list-group-item-dark">Service Price</li>
+                  <li class="list-group-item list-group-item-light">Service Duration 24 hours</li>
+                </ul>   
+              </div>
+              <div class="row justify-content-center">
+                <button type="button" className="btn btn-light w-50 green text-white fw-bolder rounded-pill mb-3">Reserve</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+
     </div>
   );
 }
