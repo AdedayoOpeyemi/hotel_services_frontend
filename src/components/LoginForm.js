aade35192207ab32fefe7e4c3dd80bfaa6fbc3be
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-11-25 11:34:03 -0300

    Add message display div to Login and Signup pages

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -26,6 +26,12 @@ const LoginForm = () => {
       </video>
       <div className="container">
         <div className="row vh-100 justify-content-center align-items-center">
+          <div className="alert alert-warning alert-dismissible fade show d-none" role="alert">
+            <strong>Holy guacamole!</strong>
+            {' '}
+            You should check in on some of those fields below.
+            <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" />
+          </div>
           <div className="col text-center text-white">
             <h1>Welcome to City Services</h1>
             <h5 className="fs-6 fw-normal">{ message }</h5>
