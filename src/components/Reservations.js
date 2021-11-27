df23ccafb6f50c95ddef1e7b988e50e47ea9ea46
Andres Castañeda <af.castaneda223@gmail.com>
2021-11-27 10:22:48 -0500

    add reservation scaffold

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Reservations() {
+  const services = useSelector((state) => state.services);
+
   return (
-    <h1 className="text-center">
-      My Reservations page
-    </h1>
+    <div className="pt-5">
+      <h1 className="text-center mb-3">My Reservations</h1>
+      { services.services
+        && services.services.map(({
+          id, name, description, imageUrl, price,
+        }) => (
+          <div className="container" key={id}>
+            <div className="card mb-3 border-0 bg-light">
+              <div className="row g-0 ">
+                <div className="col-md-4">
+                  <img src={imageUrl} className="img-fluid rounded-start w-100" alt="..." />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body text-center mt-lg-5">
+                    <h2 className="card-title mb-3">{name}</h2>
+                    <ul className="list-group border-0 mb-5">
+                      <li className="list-group-item list-group-item-light border-0 listbg mt-lg-5">{description}</li>
+                      <li className="list-group-item list-group-item-dark border-0">
+                        $
+                        {price}
+                      </li>
+                      <li className="list-group-item list-group-item-light border-0 listbg">Service Date</li>
+                      <li className="list-group-item list-group-item-dark border-0"> Service city</li>
+                    </ul>
+                    <button type="button" className="btn w-50 green text-white fw-bolder rounded-pill mt-lg-5" id={id}>Cancel Reservation</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        ))}
+      {' '}
+
+    </div>
   );
 }
 
