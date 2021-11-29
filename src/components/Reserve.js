175a74197ea98afc59f4790767d48058a6a382c7
Lucas Ferrari Soto <lcsfs11@gmail.com>
2021-11-29 15:32:20 -0300

    Implement `Reserve` button functionality.
    - [x] Add new reservation forms validations
    - [x] Finish all dropdowns
    - [x] Dispatch API post action
    - [x] Redirect to `My Reservations` after correctly booking a service

--- src/components/Reserve.js
+++ src/components/Reserve.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
 import videos from './assets/resort.mp4';
 import {
   today,
@@ -8,6 +9,9 @@ import {
   nextMonths,
   nextDays,
 } from '../utils/dates';
+import isReservationValid from '../utils/reservationValidation';
+
+import { postReservationToApi } from '../redux/reservation/reservation_duck';
 
 const yearsToDisplay = 5;
 
@@ -20,12 +24,22 @@ const findDefault = (serviceId, services) => {
 function Reserve({ serviceId }) {
   const user = useSelector((state) => state.user.user);
   const services = useSelector((state) => state.services.services);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const [newReservation, setNewReservation] = useState({
     userId: user.userId,
     serviceId,
     city: '',
     date: today,
   });
+
+  const handleCityChange = (e) => {
+    setNewReservation((reservation) => ({
+      ...reservation,
+      city: e.target.value,
+    }));
+  };
 
   const handleDateChange = (dateField) => (e) => {
     setNewReservation((reservation) => {
@@ -46,6 +60,15 @@ function Reserve({ serviceId }) {
       ...state,
       serviceId: newId,
     }));
+  };
+
+  const postNewReservation = () => {
+    const newValidation = isReservationValid(newReservation, services);
+    if (newValidation.valid) {
+      dispatch(postReservationToApi(newReservation)).then(() => {
+        navigate('/reservations');
+      });
+    }
   };
 
   return (
@@ -131,18 +154,22 @@ function Reserve({ serviceId }) {
               </div>
               <div className="col-md-3">
                 <div className="input-group mb-3 ">
-                  <select className="form-select rounded-pill btng text-white border-0" id="city">
-                    <option value="London">London</option>
-                    <option value="Melbourne">Melbourne</option>
-                    <option value="Zurich">Zurich</option>
-                    <option value="Tokyo">Tokyo</option>
-                    <option value="Auckland">Auckland</option>
-                    <option value="New York">New York</option>
+                  <select className="form-select rounded-pill btng text-white border-0" id="city" defaultValue="" onChange={handleCityChange}>
+                    <option value="" disabled>Please, select a city</option>
+                    { ['London', 'Melbourne', 'Zurich', 'Tokyo', 'Auckland',
+                      'New York'].map((city) => (
+                        <option
+                          key={`city-${city}`}
+                          value={city}
+                        >
+                          {city}
+                        </option>
+                    ))}
                   </select>
                 </div>
               </div>
               <div className="col-md-2">
-                <button type="button" className="border-0 btn btn-light w-100 textg fw-bolder rounded-pill">Reserve</button>
+                <button type="button" className="border-0 btn btn-light w-100 textg fw-bolder rounded-pill" onClick={postNewReservation}>Reserve</button>
               </div>
             </div>
           </div>
