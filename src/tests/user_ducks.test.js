97299579e625da876538863bb2f2dac11a41c29d
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-11-30 16:26:27 -0300

    create some user tests

---
+++ src/tests/user_ducks.test.js
@@ -0,0 +1,60 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import MockAdapter from 'axios-mock-adapter';
+import axios from 'axios';
+import user, {
+  getUser, postUser,
+} from '../redux/user/user_duck';
+import { testStore } from '../redux/configurateStore';
+
+const mock = new MockAdapter(axios);
+
+const port = '3000';
+const rootUrl = `http://localhost:${port}`;
+
+const defaultState = {
+  user: {
+    username: '',
+    userId: -1,
+    messages: [],
+  },
+};
+
+const store = testStore(defaultState);
+
+test('DEFAULT: should return the default state', () => {
+  expect(user(undefined, { action: 'NON_EXISTANT' })).toBe(defaultState);
+});
+
+test('GET: should set a user with name, userId, and the login message', () => {
+  const username = 'John';
+  mock.onGet(`${rootUrl}/api/v1/users`, { params: { name: username } }).reply(202, {
+    message: 'User Logged In',
+    user_id: 1,
+  });
+
+  store.dispatch(getUser(username)).then(() => {
+    expect(store.getState().user).toBe({
+      user: username,
+      userId: 1,
+      message: 'User Logged In',
+    });
+  });
+});
+
+test('POST: should set a new user with signup message', () => {
+  const username = 'Bruce';
+  mock.onPost(`${rootUrl}/api/v1/users`, { name: username }).reply(201, {
+    message: 'User was successfully created',
+    user_id: 2,
+  });
+
+  store.dispatch(postUser(username)).then(() => {
+    expect(store.getState().user).toBe({
+      message: 'User was successfully created',
+      user_id: 2,
+    });
+  });
+});
